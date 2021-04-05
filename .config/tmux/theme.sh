61c74ca8dfac5ac3423cb94f7c38dd02376c236d
Francisco <franciscodacosta285@gmail.com>
2021-04-04 21:07:00 -0300

    Updates

--- .config/tmux/theme.sh
+++ .config/tmux/theme.sh
@@ -2,7 +2,7 @@ set -g status-left-length 32
 set -g status-right-length 150
 set -g status-interval 5
 
-# default statusbar colors
+# default statusline colors
 set-option -g status-style 'fg=#202020,bg=default'
 
 # window title colors
@@ -40,17 +40,17 @@ set -g status-right '#{prefix_highlight} #[fg=#e91e63,bg=default]#(whoami)@#[fg=
 
 ### COLOR OPTIONS
 
-## Transparent
-# set-option -g pane-border-style 'fg=colour235 bg=default'
-# set-option -g pane-active-border-style fg=colour240
-# set-option -g window-active-style 'bg=default'
-# set-option -g window-style 'bg=default'
+# Transparent
+set-option -g window-style 'bg=default'
+set-option -g window-active-style 'bg=default'
+set-option -g pane-border-style 'fg=colour235 bg=default'
+set-option -g pane-active-border-style fg=colour240
 
 ## OCEANIC MATERIAL
-set-option -g window-style 'bg=#0F111A'
-set-option -g status-style 'bg=#0F111A'
-set-option -g pane-border-style 'fg=colour235 bg=#0F111A'
-set-option -g pane-active-border-style 'fg=colour240 bg=#0F111A'
+# set-option -g window-style 'bg=#0F111A'
+# set-option -g status-style 'bg=#0F111A'
+# set-option -g pane-border-style 'fg=colour235 bg=#0F111A'
+# set-option -g pane-active-border-style 'fg=colour240 bg=#0F111A'
 
 ## GRUVBOX
 # set-option -g window-style 'bg=#1d2021'
